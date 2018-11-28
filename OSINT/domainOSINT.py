d119bd8f1294f69de19aa4be88d8fcac95af4825
AlbertoJ <alberto8293@gmail.com>
2018-11-28 17:10:42 +0100

    Separate functionallity into functions

--- OSINT/domainOSINT.py
+++ OSINT/domainOSINT.py
@@ -16,6 +16,40 @@ from colorama import Fore, Back, Style
 
 #Specific libraries imports
 import dns.resolver
+import dns.exception as DNSexception
+
+#Dictionary to translate queries in human code
+DNSqueries = {
+	"A":"IPv4", 
+	"AAAA":"IPv6", 
+	"MX":"Mailservers"
+	}
+
+#Format answer of dns queries
+def formatAnswer (singleAnswer, queryType):
+	try:
+		answersResult = Fore.BLUE + "--> " + Style.RESET_ALL + DNSqueries[queryType] + ": " + Fore.BLUE + str(singleAnswer) + Style.RESET_ALL
+	except:
+		answersResult = ""
+	return answersResult
+
+#Perform simple dns queries
+def singleQuery(domain, queryType):
+	#Lists to store results
+	PrintAnswerList = []
+	try:
+		answers = dns.resolver.query(domain, queryType)
+		for rdata in answers:
+			PrintAnswerList.append(formatAnswer(rdata, queryType))
+	except dns.resolver.Timeout:
+		PrintAnswerList.append(Fore.BLUE + "--> " + Style.RESET_ALL + DNSqueries[queryType] + ": " + Fore.RED + "Timeout!!" + Style.RESET_ALL)
+	except dns.resolver.NoAnswer:
+		PrintAnswerList.append(Fore.BLUE + "--> " + Style.RESET_ALL + DNSqueries[queryType] + ": " + Fore.RED + "No answer!!" + Style.RESET_ALL)
+	except dns.resolver.NXDOMAIN:
+		PrintAnswerList.append(Fore.BLUE + "--> " + Style.RESET_ALL + DNSqueries[queryType] + ": " + Fore.RED + "No exits!!" + Style.RESET_ALL)
+
+	return PrintAnswerList
+
 
 #Description and arguments
 parser = argparse.ArgumentParser(
@@ -27,27 +61,27 @@ argumentos = parser.parse_args()
 
 #Start
 print ("\nInitiating analysis for domain=[ "
- + Fore.RED + argumentos.domain + Style.RESET_ALL+ 
+ + Fore.BLUE + argumentos.domain + Style.RESET_ALL+ 
  " ]...")
 
 #Configure google domain server
 print (Fore.BLUE + "+ " + Style.RESET_ALL + "Using "+ 
-	Fore.RED + "8.8.8.8" + Style.RESET_ALL + " nameserver")
+	Fore.BLUE + "8.8.8.8" + Style.RESET_ALL + " nameserver")
 print (Fore.BLUE + "+ " + Style.RESET_ALL + "Using "+ 
-	Fore.RED + "8.8.4.4" + Style.RESET_ALL + " nameserver")
+	Fore.BLUE + "8.8.4.4" + Style.RESET_ALL + " nameserver")
 
 resolver = dns.resolver.Resolver(configure=False)
 resolver.nameservers = ['8.8.8.8','8.8.4.4']
 
-
-#Simple query
-#To-Do capture (timeout, NXDOMAIN, NoAnswer, NoNameservers)
-
 #Check IPv4
-answers = dns.resolver.query(argumentos.domain, 'A')
-for rdata in answers:
-	print (Fore.BLUE + "--> " + Style.RESET_ALL + "Ipv4 address: " + Fore.RED + str(rdata) + Style.RESET_ALL)
+answer = singleQuery(argumentos.domain, 'A')
+for entry in answer:
+	print (entry)
 #Check IPv6
-answers = dns.resolver.query(argumentos.domain, 'AAAA')
-for rdata in answers:
-	print (Fore.BLUE + "--> " + Style.RESET_ALL + "Ipv6 address: " + Fore.RED + str(rdata) + Style.RESET_ALL)
+answer = singleQuery(argumentos.domain, 'AAAA')
+for entry in answer:
+	print (entry)
+#Check MAIL
+answer = singleQuery(argumentos.domain, 'MX')
+for entry in answer:
+	print (entry)
